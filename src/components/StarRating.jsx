798c942f5480ad4768f25f0c140cf762fdb81ef3
Rolando Medina <rolando.medina.romo@gmail.com>
2021-12-10 18:49:53 -0600

    fixing: Trying to fix a problem with the star rating component

--- src/components/StarRating.jsx
+++ src/components/StarRating.jsx
@@ -4,14 +4,37 @@ import '@componentStyles/StarRating.scss'
 import { ReactComponent as Star } from '@images/rating_star.svg';
 
 function StarRating() {
+  const [stars, setStars] = React.useState('')
+
+  // React.useEffect(() => {
+  //   console.log(stars)
+    
+  // }, [stars])
   
+  const onClick = e => {
+    const rating = parseInt(e.target.getAttribute('value'));
+    const stars = Array.from(document.getElementsByClassName('StarRating__star'))
+    console.log(rating)
+    console.log(e.target)
+    console.log(e)
+    stars.slice(0,rating).map(element => {
+      element.classList.add('on');
+      // console.log(element)
+    })
+    // stars.slice(rating+1).map(element => {
+    //   console.log(rating)
+    //   console.log(element)
+    //   element.classList.remove('on');
+    // })
+  }
+
   return (
     <div className='StarRating__container'>
-      <Star className='StarRating__star on'/>
-      <Star className='StarRating__star on'/>
-      <Star className='StarRating__star on'/>
-      <Star className='StarRating__star'/>
-      <Star className='StarRating__star'/>
+      <Star className='StarRating__star' onClick={onClick} value={1} />
+      <Star className='StarRating__star' onClick={onClick} value={2} />
+      <Star className='StarRating__star' onClick={onClick} value={3} />
+      <Star className='StarRating__star' onClick={onClick} value={4} />
+      <Star className='StarRating__star' onClick={onClick} value={5} />
     </div>
   );
 };
