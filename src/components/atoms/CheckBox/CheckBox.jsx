86419b30d16892172b3452f47469aed20cee7ef0
Rolando Medina <rolando.medina.romo@gmail.com>
2021-12-12 22:13:52 -0600

    feat: Completed the CheckBox component, now it's functional

--- src/components/atoms/CheckBox/CheckBox.jsx
+++ src/components/atoms/CheckBox/CheckBox.jsx
@@ -1,14 +1,33 @@
 import React from "react";
 
-import './CheckBox.scss';
+import "./CheckBox.scss";
+import { ReactComponent as Checkmark } from '@images/checkmark.svg'
 
 function CheckBox() {
+  const [remote, setRemote] = React.useState(false)
+
+  // useEffect(() => {
+
+  // }, []);
+  const onChange = () => {
+    setRemote(!remote);
+  }
+  
   return (
     <div className="checkbox__container">
-      <label className="checkbox__container-label">
-        <input type="checkbox" className="checkbox__container-input" />
+      <label className="checkbox__label" >
+        <div className="checkbox__label-circle">
+          <input
+            type="checkbox"
+            className="checkbox__input"
+            onChange={onChange}
+          />
+          <div className={`checkbox__box ${!remote && "unactive"}`} >
+            {!!remote && <Checkmark className="checkbox__box-checkmark" />}
+          </div>
+        </div>
+          <span>Remote</span>
       </label>
-      <span>Remote</span>
     </div>
   );
 }
