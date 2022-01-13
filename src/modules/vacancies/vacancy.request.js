908f52c27f5d5ec787a732d48d1f71be806cb1b9
Felipe Duitama <felipedc09@gmail.com>
2022-01-13 17:48:29 -0500

    feat: add delete method to delete vacancy

--- src/modules/vacancies/vacancy.request.js
+++ src/modules/vacancies/vacancy.request.js
@@ -1,9 +1,14 @@
 import getAxiosRequest from "../../utils/network/request";
 
+const buildDeleteURL =(userId, vacantId ) => {
+  return `/job-vacancies/${userId}/${vacantId}`
+}
+
 const host = `${process.env.SERVER_HOST}/api`;
 const endPoints = {
   getVacancies: { url: "/job_vacant", method: "GET" },
   createVacancy: { url: "/job_vacant", method: "POST" },
+  createVacancy: { url: buildDeleteURL, method: "DELETE" },
 };
 
 async function postVacancy(vacancy) {
@@ -31,4 +36,16 @@ async function getVacancies() {
   return data;
 }
 
-export { postVacancy, getVacancies as getVacancy };
+async function deleteVacancy(userId, vacantId) {
+  let response = null;
+  const {  url, method } = endPoints.getVacancies;
+  response = await getAxiosRequest({
+    method,
+    url: `${host}${url(userId, vacantId)}`,
+  });
+  const { data } = response;
+  if (!data) throw new SystemException("", "error");
+  return data;
+}
+
+export { postVacancy, getVacancies as getVacancy, deleteVacancy as deleteVacancies };
