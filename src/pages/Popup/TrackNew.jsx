99c34cd9ef22e1960558eaac4b5d412f9a8368af
Felipe Duitama <felipedc09@gmail.com>
2022-03-27 19:45:07 -0500

    feat: add alerts in form

--- src/pages/Popup/TrackNew.jsx
+++ src/pages/Popup/TrackNew.jsx
@@ -1,5 +1,4 @@
-
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Layout } from '../../components/templates/Layout/Layout'
 import { ResponsiveNavBar } from '../../components/organisms/Navbar/Navbar'
 import { StarRating } from '../../components/molecules/StarRating/StarRating'
@@ -7,51 +6,78 @@ import { TrackForm } from '../../components/organisms/TrackForm/TrackForm'
 import { getUrl } from 'Helpers/pageScraping.js'
 import { postVacancy } from '../../modules/vacancies/vacancy.request'
 import { useAuth0 } from '@auth0/auth0-react'
-import { validateObj } from "../../utils/validations/inputValidation";
-
+import { validateObj } from '../../utils/validations/inputValidation'
+import CustomizedSnackbars from '../../components/organisms/CustomSnackbar/CustomSnackbar'
 
 function TrackNew() {
-	const [inputValue, setInputValue] = React.useState({})
-	const [rating, setRating] = React.useState('3')
+	const [inputValue, setInputValue] = useState({})
+	const [rating, setRating] = useState('3')
 	const { getAccessTokenSilently } = useAuth0()
+	const [alert, setAlert] = useState({ message: '', type: 'error' })
+	const [open, setOpen] = useState(false)
 
+	async function sendFormData(event) {
+		console.log(inputValue.remote)
+		event.preventDefault()
+		const vacancyToCreate = {
+			title: inputValue.position,
+			link: inputValue.link,
+			company: inputValue.company,
+			salary_from: inputValue.offeredSalary,
+			salary_to: inputValue.offeredSalary + 10,
+			currency: inputValue.currency,
+			date_application: new Date().toISOString().slice(0, 10),
+			interest: parseInt(rating),
+			notes: inputValue.notes,
+			user_id: 1,
+			remote: !!inputValue.remote,
+			status: 'interested',
+		}
 
-  async function sendFormData(event) {
-    event.preventDefault();
-    const vacancyToCreate = {
-      title: inputValue.position,
-      link: inputValue.link,
-      company: inputValue.company,
-      salary_from: inputValue.offeredSalary,
-      salary_to: inputValue.offeredSalary + 10,
-      currency: inputValue.currency,
-      date_application: new Date().toISOString().slice(0,10),
-      interest: parseInt(rating),
-      notes: inputValue.notes,
-      user_id: 1,
-      remote: inputValue.remote,
-      status: "interested"
-    };
-    console.log(vacancyToCreate);
-    const validation = validateObj("vacancySchema", vacancyToCreate);
-    if (validation === "ok") {
-      const token = await getAccessTokenSilently()
-      let result = await postVacancy(vacancyToCreate, token)
-      if (resulta.data) {
-        handleCreateVacacySuccess()
-      }
-    } else {
-      console.log(validation)
-    }
-  }
+		const validation = validateObj('vacancySchema', vacancyToCreate)
+		setVacancy(validation, vacancyToCreate)
+	}
 
-	React.useEffect(async () => {
+	const activeAlert = (message, type) => {
+		setAlert({ message, type })
+		setOpen(true)
+	}
+
+	const ShowValidationErrors = (validations) => {
+		let currentValidations = 'You have the follow errors: \n\r'
+		validations.forEach((validation) => {
+			currentValidations += `${validation.key}: ${validation.message}. \n\r`
+		})
+		activeAlert(currentValidations, 'error')
+	}
+
+	useEffect(async () => {
 		const response = await getUrl()
 		setInputValue({
 			...inputValue,
 			...response,
 		})
 	}, [])
+
+	const setVacancy = async (validation, vacancyToCreate) => {
+		try {
+			if (validation === 'ok') {
+				const token = await getAccessTokenSilently()
+				console.log('token', token)
+				let result = await postVacancy(vacancyToCreate, token)
+				if (result) {
+					activeAlert(
+						'The vacancy  was registered successfully',
+						'success'
+					)
+				}
+			} else {
+				ShowValidationErrors(validation)
+			}
+		} catch (error) {
+			activeAlert(error, 'error')
+		}
+	}
 
 	return (
 		<Layout>
@@ -63,6 +89,12 @@ function TrackNew() {
 					onSubmit={sendFormData}>
 					<StarRating rating={rating} setRating={setRating} />
 				</TrackForm>
+				<CustomizedSnackbars
+					type={alert.type}
+					message={alert.message}
+					status={open}
+					resetOpen={setOpen}
+				/>
 			</div>
 		</Layout>
 	)
